81aa9080f8934efda45188762006a254ba048f1a
classypills <classypills@gmail.com>
2022-10-15 06:44:12 -0700

    third commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
-  #include <stdio.h>
-   /** * main - Prints "Programming is like building a multilingual * puzzle, followed by a new line. * * Return: Always 0. */
-int main(void){	puts("\"Programming is like building a multilingual puzzle");
-return (0);}
+Type in the following lines of code
+
+#include <stdio.h>
+/**
+ *main- Serves as the entry point for the program
+ *
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
